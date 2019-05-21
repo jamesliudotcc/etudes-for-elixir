544aadca108d72ae49fdc292f628c38307d48519
jamesliudotcc <james@jamesliu.cc>
2019-05-21 11:00:22 -0700

    basic pattern matching working

--- geom.ex
+++ geom.ex
@@ -1,9 +1,28 @@
 defmodule Geom do
   @doc """
-  Calculates the area of a rectangle with x width and y height. Assumes that a dimension is 1 if it is not given.
+  Calculates the area of a shape with x width and y height.
+
+  Shapes are passed as atoms, so far :rectangle, :triangle, and :ellipse are implemented
+
+  :ellipse assumes dimensions are radii
+
+  Assumes that a dimension is 1 if it is not given.
+
+  Assumes :rectangle if no shape is given.
 
   """
-  def area(x \\ 1, y \\ 1) do
+  def area(shape \\ :rectangle, x \\ 1, y \\ 1)
+
+  def area(:rectangle, x, y) do
     x * y
+  end
+
+  def area(:triangle, x, y) do
+    x * y * 0.5
+  end
+
+  def area(:ellipse, x, y) do
+    # x and y are radii
+    x * y * 3.14
   end
 end
