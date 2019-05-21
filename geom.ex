c39d6e544d90ca21388730b16cf625806f8cb8f0
jamesliudotcc <james@jamesliu.cc>
2019-05-21 09:30:28 -0700

    added doc to geom.ex

--- geom.ex
+++ geom.ex
@@ -1,5 +1,9 @@
 defmodule Geom do
-    def area(x, y) do
+    @doc """
+    Calculates the area of a rectangle with x width and y height. Assumes that a dimension is 1 if it is not given.
+    
+    """
+    def area(x \\ 1, y \\ 1) do
         x * y
     end
 end
